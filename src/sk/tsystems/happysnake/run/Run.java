6247880c4293a74c648fd65f79f534449abdc15e
student <student@DESKTOP-DKDBLTK>
2017-10-24 09:23:17 +0200

    create Frame with fullScrean, add GamePanel

--- src/sk/tsystems/happysnake/run/Run.java
+++ src/sk/tsystems/happysnake/run/Run.java
@@ -1,9 +1,29 @@
 package sk.tsystems.happysnake.run;
 
-public class Run {
+import javax.swing.JFrame;
+
+import sk.tsystems.happysnake.gamepanel.GamePanel;
+
+public class Run extends JFrame {
+
+	public Run() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		setContentPane(new GamePanel());
+		
+		/* set fullScrean */
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true);
+
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Snake core");
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Run().setVisible(true);
+			}
+		});
 
 	}
 
