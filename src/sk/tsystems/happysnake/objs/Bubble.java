c150f0615fddea89d7a276437151c68239a8e711
student <student@DESKTOP-DKDBLTK>
2017-10-24 09:45:48 +0200

    Bubble rnd color

--- src/sk/tsystems/happysnake/objs/Bubble.java
+++ src/sk/tsystems/happysnake/objs/Bubble.java
@@ -1,16 +1,29 @@
 package sk.tsystems.happysnake.objs;
 
+import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.util.Random;
 
 public class Bubble extends Ellipse2D.Double {
 	private static final long serialVersionUID = -370534684811157194L;
 
-	public static int WIDTH;
-	public static int HEIGHT;
+	public static int SCR_W;
+	public static int SCR_H;
 	private static int SIZE = 15;
 
 	private static final Random RND = new Random();
+
+	private static double nV(int max) {
+		return RND.nextInt(max);
+	}
+
+	private static int nIB() {
+		return RND.nextInt(255);
+	}
+
+	private Color color;
+	private long appeared;
+	private boolean consumed;
 
 	/**
 	 * Creates new bubble on random screen position
@@ -18,11 +31,16 @@ public class Bubble extends Ellipse2D.Double {
 	 * @param pt
 	 */
 	public Bubble() {
-		super(nV(WIDTH), nV(HEIGHT), SIZE, SIZE);
+		super(nV(SCR_W), nV(SCR_H), SIZE, SIZE);
+		appeared = System.currentTimeMillis();
+		color = new Color(nIB(), nIB(), nIB());
 	}
 
-	private static double nV(int max) {
-		return RND.nextInt(max);
+	public Color getColor() {
+		return color;
 	}
 
+	public boolean isConsumed() {
+		return consumed;
+	}
 }
