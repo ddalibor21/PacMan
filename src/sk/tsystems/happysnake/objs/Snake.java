b358110ea4456f46db734cbe877a69e62c935349
student <student@DESKTOP-DKDBLTK>
2017-10-24 10:08:24 +0200

    Snake base class

---
+++ src/sk/tsystems/happysnake/objs/Snake.java
@@ -0,0 +1,38 @@
+package sk.tsystems.happysnake.objs;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Snake {
+	private List<Point> points;
+	private Point head;
+	private int size;
+
+	public Snake() {
+		super();
+		points = new LinkedList<>();
+		size = 2;
+	}
+
+	public void draw(Graphics2D g) {
+		g.setColor(Color.BLACK);
+		for (Point p : points) {
+			g.fillOval(p.x, p.y, 10, 10);
+		}
+
+		g.setColor(Color.GREEN);
+		g.fillOval(head.x, head.y, 10, 10);
+	}
+
+	public void move(Point pt) {
+		if (points.size() > size)
+			points.remove(0);
+
+		points.add(pt);
+		head = pt;
+	}
+
+}
